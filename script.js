30471f00f7cc4c080fd74038cee9f01ed848f760
PingHuskar <ping47024@icloud.com>
2022-06-20 01:21:24 +0700

    add custom bgColor, logo, title

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const searchParam = new URLSearchParams(window.location.search)
-const t1 = searchParam.get('t1')
-const t2 = searchParam.get('t2')
-const t1Name = searchParam.get('t1name')
-const t2Name = searchParam.get('t2name')
+const t1 = searchParam.get('score1')
+const t2 = searchParam.get('score2')
+const t1Name = searchParam.get('name1')
+const t2Name = searchParam.get('name2')
+const title = searchParam.get('title') !== null ? searchParam.get('title') : "Scoreboard App"
+document.title = title
+document.getElementById('logo1').src = searchParam.get('logo1') !== null ? searchParam.get('logo1') : 'https://seeklogo.net/wp-content/uploads/2012/12/manchester-united-logo-vector-400x400.png'
+document.getElementById('logo2').src = searchParam.get('logo2') !== null ? searchParam.get('logo2') : 'https://seeklogo.net/wp-content/uploads/2011/08/liverpool-logo-vector.png'
+var bg = searchParam.get('bg') !== null ? searchParam.get('bg') : 'black'
+if (/[\da-f]{7,}/i.test(bg)) {
+    bg = bg.slice(0,6)
+}
+if (/[\da-f]{6}|[\da-f]{3}/i.test(bg)) {
+    document.documentElement.style.setProperty('--bg-color', `#${bg}`);
+}
+else {
+    document.documentElement.style.setProperty('--bg-color', `${bg}`);
+}
+const shadow = searchParam.get('shadow') !== null ? searchParam.get('shadow') : 'blue'
+document.documentElement.style.setProperty('--shadow-color', shadow);
 var t1score = t1 !== null ? t1.replace('?','') : 0
 var t2score = t2 !== null ? t2.replace('?','') : 0
 document.getElementById('Team1Score').textContent = t1score;
@@ -10,22 +26,22 @@ document.getElementById('Team2Score').textContent = t2score;
 document.getElementById('t1Name').textContent = t1Name !== null ? t1Name : 'Team 1'
 document.getElementById('t2Name').textContent = t2Name !== null ? t2Name : 'Team 2'
 var currentURL = `${location}`
-document.getElementById('Team1Score').addEventListener('click', () => {
+document.getElementById('logo1').addEventListener('click', () => {
     if (t1 !== null) {
-        open(currentURL.replace(`t1=${t1score}`,`t1=${parseInt(t1score)+1}`), "_self")
+        open(currentURL.replace(`score1=${t1score}`,`score1=${parseInt(t1score)+1}`), "_self")
     }
     else {
-        currentURL += `?&t1=1`
+        currentURL += `?&score1=1`
         open(currentURL, "_self")
     }
 })
-document.getElementById('Team2Score').addEventListener('click', () => {
+document.getElementById('logo2').addEventListener('click', () => {
     if (t2 !== null) {
-        open(currentURL.replace(`t2=${t2score}`,`t2=${parseInt(t2score)+1}`), "_self")
+        open(currentURL.replace(`score2=${t2score}`,`score2=${parseInt(t2score)+1}`), "_self")
     }
     else {
-        currentURL += `?&t2=1`
+        currentURL += `?&score2=1`
         open(currentURL, "_self")
     }
-   
 })
+
