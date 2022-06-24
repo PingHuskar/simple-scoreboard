268e218795961563405cde09fe158abf2651f05e
PingHuskar <ping47024@icloud.com>
2022-06-24 11:09:51 +0700

    clean code

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const t1 = searchParam.get('score1')
 const t2 = searchParam.get('score2')
 const t1Name = searchParam.get('name1')
 const t2Name = searchParam.get('name2')
-const title = searchParam.get('title') !== null ? searchParam.get('title') : "Scoreboard App"
+const title = searchParam.get('title') || "Scoreboard App"
 document.title = title
-document.getElementById('logo1').src = searchParam.get('logo1') !== null ? searchParam.get('logo1') : 'https://seeklogo.net/wp-content/uploads/2012/12/manchester-united-logo-vector-400x400.png'
-document.getElementById('logo2').src = searchParam.get('logo2') !== null ? searchParam.get('logo2') : 'https://seeklogo.net/wp-content/uploads/2011/08/liverpool-logo-vector.png'
-var bg = searchParam.get('bg') !== null ? searchParam.get('bg') : 'black'
-var fontColor = searchParam.get('c') !== null ? searchParam.get('c') : 'white'
-var sc = searchParam.get('sc') !== null ? searchParam.get('sc') : 'white'
+document.getElementById('logo1').src = searchParam.get('logo1') || 'https://seeklogo.net/wp-content/uploads/2012/12/manchester-united-logo-vector-400x400.png'
+document.getElementById('logo2').src = searchParam.get('logo2') || 'https://seeklogo.net/wp-content/uploads/2011/08/liverpool-logo-vector.png'
+var bg = searchParam.get('bg') || 'black'
+var fontColor = searchParam.get('c') || 'white'
+var sc = searchParam.get('sc') || 'white'
 function setColor(color,prop) {
     if (/[\da-f]{7,}/i.test(color)) {
         color = color.slice(0,6)
@@ -25,14 +25,14 @@ setColor(bg,'bg')
 setColor(fontColor,'font')
 setColor(sc,'sc')
 
-const shadow = searchParam.get('shadow') !== null ? searchParam.get('shadow') : 'blue'
+const shadow = searchParam.get('shadow') || 'blue'
 document.documentElement.style.setProperty('--shadow-color', shadow);
-var t1score = t1 !== null ? t1.replace('?','') : 0
-var t2score = t2 !== null ? t2.replace('?','') : 0
+var t1score = t1 || 0
+var t2score = t2 || 0
 document.getElementById('Team1Score').textContent = t1score;
 document.getElementById('Team2Score').textContent = t2score;
-document.getElementById('t1Name').textContent = t1Name !== null ? t1Name : 'Team 1'
-document.getElementById('t2Name').textContent = t2Name !== null ? t2Name : 'Team 2'
+document.getElementById('t1Name').textContent = t1Name || 'Team 1'
+document.getElementById('t2Name').textContent = t2Name || 'Team 2'
 var currentURL = `${location}`
 document.getElementById('logo1').addEventListener('click', () => {
     if (t1 !== null) {
